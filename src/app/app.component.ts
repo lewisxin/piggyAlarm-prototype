c56a2238ec09ba576cd267a30960eedeaffeebf3
Liu Xin <lewisxin718@gmail.com>
2017-04-09 04:18:28 +0800

    Update status bar style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleBlackOpaque();
+      statusBar.backgroundColorByHexString('#142465');      
       splashScreen.hide();
     });
   }
